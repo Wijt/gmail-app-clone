5aa5c4807de677b29d95acf99562587eaceeaf1d
Furkan Kaya <8fkaya@gmail.com>
2021-04-20 19:01:34 +0300

    Bottom Navigation Bar added.

--- app/src/main/java/com/example/vizesinavi/MainActivity.java
+++ app/src/main/java/com/example/vizesinavi/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
+        BadgeDrawable badge = bottomNavigation.getOrCreateBadge(R.id.menu_1);
+        badge.setNumber(28);
     }
+
+
 }
